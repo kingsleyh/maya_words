0f59259255c01c01920324858fc34d178e0ea6d9
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-03-16 15:36:43 +0000

    add words

--- src/Models.elm
+++ src/Models.elm
@@ -16,7 +16,40 @@ newModel : Model
 newModel =
     let
         words =
-            [ "RUN", "WALK", "SING", "SIT", "STAND", "EAT", "LOVE", "RACE", "OF", "THE" ]
+            [ "THE", "AND", "A"
+             , "TO", "SAID", "IN"
+             , "HE", "I", "OF"
+             , "IT", "WAS", "YOU"
+             , "THEY", "ON", "SHE"
+             , "IS", "FOR", "AT"
+             , "HIS", "BUT", "THAT"
+             , "WITH", "ALL", "AN"
+             , "SO", "WE", "CAN"
+             , "ARE", "UP", "HAD"
+             , "MY", "HER", "WHAT"
+             , "THERE", "OUT", "THIS"
+             , "HAVE", "WENT", "BE"
+             , "LIKE", "SOME", "NOT"
+             , "THEN", "WERE", "GO"
+             , "LITTLE", "AS", "NO"
+             , "SAW", "MAKE", "MUM"
+             , "ONE", "THEM", "DO"
+             , "ME", "DOWN", "DAD"
+             , "BIG", "WHEN", "ITS"
+             , "SEE", "LOOKED", "VERY"
+             , "LOOK", "DONT", "COME"
+             , "WILL", "INTO", "BACK"
+             , "FROM", "CHILDREN", "HIM"
+             , "MR", "OFF", "ASKED"
+             , "GET", "JUST", "NOW"
+             , "CAME", "OH", "ABOUT"
+             , "GOT", "THEIR", "PEOPLE"
+             , "YOUR", "PUT", "COULD"
+             , "HOUSE", "OLD", "TOO"
+             , "BY", "DAY", "MADE"
+             , "TIME", "IM", "IF"
+             , "HELP", "MRS", "CALLED"
+             ]
 
         letters =
             String.split "" (Maybe.withDefault "" (LE.getAt 0 words))
